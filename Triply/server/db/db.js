63bff69ce9f591c44c7747e96ee1dbf2d0647d97
Vaideesh Siva <vaideesh4@gmail.com>
2025-09-11 23:19:19 -0500

    Update db.js

--- Triply/server/db/db.js
+++ Triply/server/db/db.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "sys",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "sys",
   connectionLimit: 10
 });
 
