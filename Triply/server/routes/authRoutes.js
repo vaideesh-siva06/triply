fd9a4c319e211cf53b3c195622a2d13ec39b8c4b
Vaideesh Siva <vaideesh4@gmail.com>
2025-09-12 15:04:56 -0500

    Update authRoutes.js

--- Triply/server/routes/authRoutes.js
+++ Triply/server/routes/authRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.delete("/users/:id", authMiddleware, deleteUser);
+router.put("/users/:id", authMiddleware, updateUser);
 
 export default router;
